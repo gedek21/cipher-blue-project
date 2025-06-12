5304cccd3f4cbd5cb8485c16a9f4a45833c8661a
gede kitchen batara <you@example.com>
2025-06-12 22:11:27 +0900

    add cmake to project to learning build an app with cmake

---
+++ CMakeFiles/cipher-blue-project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cipher-blue-project.dir/link.d"
+  "CMakeFiles/cipher-blue-project.dir/src/main.c.o"
+  "CMakeFiles/cipher-blue-project.dir/src/main.c.o.d"
+  "cipher-blue-project"
+  "cipher-blue-project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cipher-blue-project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/utils.dir/src/utils.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/utils.dir/src/utils.c.o: \
+ /home/gedekb/Documents/cipher-blue-project/src/utils.c \
+ /usr/include/stdc-predef.h
